48074fe4b508132e0c065eb284a2b3dce4577bde
HigorSouza <higor.andrade@hotmail.com>
2022-10-26 17:54:05 -0300

    Removendo secret key

--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0bheh(^4mwnh_&b=+qga@c6#=5$n!$z0w=@a$jjfsu1vfp!!)0'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
